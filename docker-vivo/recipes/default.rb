ab9de655a3a2e4bbecb19e883290205277c277e1
Felipe Ribeiro <fribeiro@avenuecode.com>
2015-12-04 15:36:06 -0200

    docker cookbook recipes

--- docker-vivo/recipes/default.rb
+++ docker-vivo/recipes/default.rb
@@ -13,12 +13,18 @@ docker_service 'default' do
   action [:create, :start]
 end
 
-#docker_image 'busybox' do
-#  action :pull
-#end
+docker_image 'busybox' do
+  action :pull
+end
 
 #docker_container 'an echo server' do
 #  repo 'busybox'
 #  port '1234:1234'
 #  command "nc -ll -p 1234 -e /bin/cat"
 #end
+
+docker_container 'busybox_ls' do
+  repo 'busybox'
+  command 'ls -la /'
+  action :run
+end

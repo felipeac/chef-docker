0fcafed96e2ed2414570b45dce4fe543649dcb20
Felipe Ribeiro <fribeiro@avenuecode.com>
2015-12-04 14:46:11 -0200

    docker-vivo commit

---
+++ docker-vivo/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: docker-vivo
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "docker"
+
+docker_service 'default' do
+  action [:create, :start]
+end
+
+#docker_image 'busybox' do
+#  action :pull
+#end
+
+#docker_container 'an echo server' do
+#  repo 'busybox'
+#  port '1234:1234'
+#  command "nc -ll -p 1234 -e /bin/cat"
+#end
